4808a644f32531b019e4fc293f9638ff35fbe86b
DamonJavert <damonjavert@protonmail.com>
2018-06-18 22:28:53 +0700

    Added torrent links
    Needs tidyup though

--- jps2sm.py
+++ jps2sm.py
@@ -7,7 +7,7 @@ import urllib2
 from bs4 import BeautifulSoup
 import re
 
-jps_page = "file:///home/***REMOVED***/Downloads/minjps.html"
+jps_page = "file:///home/***REMOVED***/Downloads/sgbuono.html"
 page = urllib2.urlopen(jps_page)
 soup = BeautifulSoup(page, 'html5lib')
 
@@ -46,17 +46,33 @@ print artist
 title = re.findall('<a.*> - (.*) \[', text)
 print title
 
+rel2 = str(soup.select('#content .thin .main_column .torrent_table tbody')[0])
+#print rel2
+rel2data = re.findall('\\xbb.* (.*) / (.*) / (.*)</a>', rel2)
+#print rel2data[0]
+
+torrentlinks = re.findall('href="(.*)" title="Download"', rel2)
+#print torrentlinks[0]
+
+for releasedata, torrentlink in zip(rel2data, torrentlinks):
+    print releasedata[0] , releasedata[1], releasedata[2]
+    print torrentlink
+
+"""
 release = soup.select('.torrent_table tbody tr.group_torrent td')
 #print release
 release_text = str(release[0])
+print release_text
 
-releasedata = re.search('\\xbb.* (.*) / (.*) / (.*)</a>', release_text).groups()
+
+releasedata = re.findall('\\xbb.* (.*) / (.*) / (.*)</a>', release_text)
 mformat = releasedata[0]
 bitrate = releasedata[1]
 media = releasedata[2]
 print mformat
 print bitrate
 print media
+"""
 
 groupdescription = remove_html_tags(str(soup.select('#content .thin .main_column .box .body')[0]))
 print groupdescription
