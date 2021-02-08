9c5bc86cffaa1577092c0e9e05eadbf3f1165e1c
DamonJavert <damonjavert@protonmail.com>
2021-02-08 20:01:32 +0700

    Tests improvements
    * Add test for batchmode logic error if non-complete page range
    * Other improvements to tests.sh

--- tests/tests.sh
+++ tests/tests.sh
@@ -5,11 +5,17 @@ echo "Running tests on $(git rev-parse --verify HEAD)"
 
 interpreter="python3.8"
 
-diff -u tests/group-111284 <(python3.8 jps2sm.py --urls "https://jpopsuki.eu/torrents.php?id=111284" -dn || echo "jps2sm exited non-zero!")
+#Test if start page specified but no end page error
+diff -u tests/arg-check-batchuser-no-end-page <(${interpreter} jps2sm.py -d --batchuser 194033 --batchuploaded -s 10 -n 2>&1 && echo "jps2sm exited OK but it should have errored!")
 echo ---------------------------------
+#Test general whole group AKB48 - 1830m
+diff -u tests/group-111284 <(${interpreter} jps2sm.py --urls "https://jpopsuki.eu/torrents.php?id=111284" -dn || echo "jps2sm exited non-zero!")
+echo ---------------------------------
+#Test specific torrent in a group that has Hi-Res FLAC
 diff -u tests/group-321079-torrent-472015 <(${interpreter} jps2sm.py --urls "https://jpopsuki.eu/torrents.php?id=321079&torrentid=472015" -dn || echo "jps2sm exited non-zero!")
 echo ---------------------------------
-diff -u tests/group-251299-torrent-358233 <(python3.8 jps2sm.py --urls "https://jpopsuki.eu/torrents.php?id=251299&torrentid=358233" -dn || echo "jps2sm exited non-zero!")
+#Test V.A. group that has contrib artists with no original character name
+diff -u tests/group-251299-torrent-358233 <(${interpreter} jps2sm.py --urls "https://jpopsuki.eu/torrents.php?id=251299&torrentid=358233" -dn || echo "jps2sm exited non-zero!")
 echo ---------------------------------
 echo "Tests complete"
 
