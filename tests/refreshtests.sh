5e3d0a42ce1796655bf68865ec0e3f7bdf6d5a84
DamonJavert <damonjavert@protonmail.com>
2021-07-04 19:48:45 +0700

    Upddate tests and refreshtests.sh

---
+++ tests/refreshtests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+jps2sm=$1
+
+$jps2sm --urls "https://jpopsuki.eu/torrents.php?id=111284" -dn | tee tests/group-111284
+$jps2sm -d --batchuser 194033 --batchuploaded -s 10 -n 2>&1 | tee tests/arg-check-batchuser-no-end-page
+$jps2sm -dn --batchuser 194033 --batchuploaded -s 15 -e 16 | tee tests/batchuser-194033-zilla-pages-15-16
+$jps2sm -d --batchuser 194033 --batchuploaded -s 10 -n | tee tests/arg-check-batchuser-no-end-page
+$jps2sm --urls "https://jpopsuki.eu/torrents.php?id=251299&torrentid=358233" -dn | tee tests/group-251299-torrent-358233
+$jps2sm --urls "https://jpopsuki.eu/torrents.php?id=321079&torrentid=472015" -dn | tee tests/group-321079-torrent-472015
